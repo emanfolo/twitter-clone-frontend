75430343f49553b8c8654d8e5b31b4fef105c870
Emmanuel <emmanuelfolorunsho@icloud.com>
2021-12-16 16:25:48 +0000

    changed user creation from graphql to express

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,94 +1,85 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery } from "@apollo/client";
 
-const AllTweetsQuery = gql `
-  query{
-    tweets{
-    id
-    contents
-    createdAt
-    image
-    user {
+const AllTweetsQuery = gql`
+  query {
+    tweets {
       id
-      username
-      name
-      profile{
-        image
+      contents
+      createdAt
+      image
+      user {
+        id
+        username
+        name
+        profile {
+          image
+        }
+      }
+      hashtags {
+        contents
+        id
       }
     }
-    hashtags {
-      contents
-      id
-    }
-   }
   }
-  
-`
+`;
 
 interface User {
-  id: number, 
-  name: string,
-  username: string,
-  createdAt?: string,
-  profile: Profile,
-  tweets?: Array<Tweet>,
-  followedBy?: Array<User>
-  following: Array<User>
+  id: number;
+  name: string;
+  username: string;
+  createdAt?: string;
+  profile: Profile;
+  tweets?: Array<Tweet>;
+  followedBy?: Array<User>;
+  following: Array<User>;
 }
 
 interface Profile {
-  id?: number, 
-  image?: string,
-  header_image?: string,
-  bio?: string,
+  id?: number;
+  image?: string;
+  header_image?: string;
+  bio?: string;
 }
 
 interface Hashtag {
-  id: number,
-  contents: string, 
-  tweets?: Tweet
+  id: number;
+  contents: string;
+  tweets?: Tweet;
 }
 
 interface Tweet {
-  contents?: string,
-  createdAt: Date,
-  hashtags: Array<Hashtag>, 
-  id: number, 
-  image?: string,
-  user: User
+  contents?: string;
+  createdAt: Date;
+  hashtags: Array<Hashtag>;
+  id: number;
+  image?: string;
+  user: User;
 }
 
 const Home = () => {
+  const { data, error, loading } = useQuery(AllTweetsQuery);
 
-  const {data, error, loading} = useQuery(AllTweetsQuery);
+  if (loading) return <p> Loading....</p>;
 
-  if(loading) return <p> Loading....</p>
-
-  if(error) return <p> Ooops, something went wrong {error.message} </p>
+  if (error) return <p> Ooops, something went wrong {error.message} </p>;
 
   // console.log(data)
 
   const tweetFeed = data.tweets.map((tweet: Tweet) => (
-     <> 
-      <div >
-          {tweet.contents}
-      </div>
+    <>
+      <div>{tweet.contents}</div>
       <div>
-          <strong>{tweet.user.name}</strong> @{tweet.user.username}
-      </div>  
+        <strong>{tweet.user.name}</strong> @{tweet.user.username}
+      </div>
     </>
-  ))
-  
-  
+  ));
 
+  return (
+    <>
+      <h1>This will be the home page with tweets</h1>
+      <div>{tweetFeed}</div>
+    </>
+  );
+};
 
-  return <> 
-  <h1>
-    This will be the home page with tweets 
-  </h1>
-  <div>
-    {tweetFeed} 
-  </div>
-  </>
-}
-
-export default Home
+export default Home;
--- pages/user/new.tsx
+++ pages/user/new.tsx
@@ -1,130 +1,164 @@
-import { gql, useMutation } from '@apollo/client';
-import { useState } from 'react';
-import bcryptjs from 'bcryptjs'
+// import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+// import * as dotenv from 'dotenv'
+// // dotenv.config({path: '../../.env'})
 
-const REGISTER_USER = gql`
-    mutation Mutation($data: UserCreateInput!) {
-  createUser(data: $data) {
-    id
-    email
-    username
-    createdAt
-    profile {
-      image
-    }
-    followedBy {
-      id
-    }
-    following {
-      id
-    }
-  }
-}
+// dotenv.config()
 
-`
+// import bcryptjs from "bcryptjs";
 
-
+// const REGISTER_USER = gql`
+//   mutation Mutation($data: UserCreateInput!) {
+//     createUser(data: $data) {
+//       id
+//       email
+//       username
+//       createdAt
+//       profile {
+//         image
+//       }
+//       followedBy {
+//         id
+//       }
+//       following {
+//         id
+//       }
+//     }
+//   }
+// `;
 
 const CreateUser = () => {
+  const [emailInputState, setEmailInputState] = useState("");
+  const [usernameInputState, setUsernameInputState] = useState("");
+  const [nameInputState, setNameInputState] = useState("");
+  const [passwordInputState, setPasswordInputState] = useState("");
 
-  const [emailInputState, setEmailInputState] = useState('')
-  const [usernameInputState, setUsernameInputState] = useState('')
-  const [nameInputState, setNameInputState] = useState('')
-  const [passwordInputState, setPasswordInputState] = useState('')
+  // const [registerMutateFunction, { data, loading, error }] =
+  //   useMutation(REGISTER_USER);
 
-  const [registerMutateFunction, { data, loading, error }] = useMutation(REGISTER_USER)
+  // const encrypt = (password: string) => {
+  //   let hashedPassword = bcryptjs.hash(password, 12);
+  //   return hashedPassword;
+  // };
 
-  const encrypt = (password: string) => {
-    let hashedPassword = bcryptjs.hash(password, 12)
-    return hashedPassword
+  // const sendParams = async () => {
+  //   let dbPass = null;
+
+  //   dbPass = await encrypt(passwordInputState);
+
+  //   console.log(dbPass);
+
+  //   if (dbPass !== null) {
+  //     registerMutateFunction({
+  //       variables: {
+  //         data: {
+  //           email: emailInputState,
+  //           name: nameInputState,
+  //           username: usernameInputState,
+  //           profile: {
+  //             create: {
+  //               image: null,
+  //               header_image: null,
+  //               bio: null,
+  //             },
+  //           },
+  //           password: dbPass,
+  //         },
+  //       },
+  //     });
+  //   }
+  // };
+
+  const checkInputs = () => {
+   if (!emailInputState.includes('@') || emailInputState.length < 5){
+      console.log('Invalid params, please enter in your full details')
+      return false
+    } else if (passwordInputState.length < 8){
+      console.log('Invalid params, please enter in your full details')
+      return false
+    } else if (usernameInputState.length < 4) {
+      console.log('Invalid params, please enter in your full details')
+      return false
+    } else if (nameInputState.length < 1){
+      console.log('Invalid params, please enter in your full details')
+      return false
+    } else {
+      return true
+    }
   }
 
   const sendParams = async () => {
-
-    let dbPass = null
-
-    dbPass = await encrypt(passwordInputState)
-
-    console.log(dbPass)
-
-
-    if(dbPass !== null){
-      registerMutateFunction({ variables: { 
-            data: {
-                    "email": emailInputState,
-                    "name": nameInputState,
-                    "username": usernameInputState,
-                    "profile": {
-                      "create": {
-                        "image": null,
-                        "header_image": null,
-                        "bio": null
-                      }
-                    },
-                    "password": dbPass
-                  }
-                } 
-              });
-    }
-    
-  }
+    if (checkInputs()) {
+      const response = await fetch('http://localhost:4000/user/register', {
+        method: 'POST',
+        headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },
+        body: JSON.stringify( {
+          email: emailInputState,
+          password: passwordInputState,
+          name: nameInputState,
+          username: usernameInputState
+       })
+      })
+      const result = await response.json()
+      console.log(result)
+  };
+}
 
   // if (loading) return 'Loading...';
 
-  if (error) return `Error! ${error.message}`;
-
-  if(data) return <> {data.id} {data.username} {data.name} </>
-
-
-
+  // if (error) return `Error! ${error.message}`;
 
   return (
-  <> 
-  Sign up page 
-  <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          sendParams()
-        }}
-      >
-        <label>
-          Email:
-        <input
-          onChange={(e) =>{
-            setEmailInputState(e.target.value)
+    <>
+      Sign up page
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendParams();
           }}
-        /> 
-        </label>
-        <label>
-        Username:
-        <input
-          onChange={(e) =>{
-            setUsernameInputState(e.target.value)
-          }}
-        /> 
-        </label>
-        <label>
-          Password:
-        <input
-          onChange={(e) =>{
-            setPasswordInputState(e.target.value)
-          }}
-        /> 
-        </label>
-        <label>Name:
-        <input
-          onChange={(e) =>{
-            setNameInputState(e.target.value)
-          }}
-        />
-        </label>
+        >
+          <label>
+            Email:
+            <input
+              onChange={(e) => {
+                setEmailInputState(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Username:
+            <input
+              onChange={(e) => {
+                setUsernameInputState(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              onChange={(e) => {
+                setPasswordInputState(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Name:
+            <input
+              onChange={(e) => {
+                setNameInputState(e.target.value);
+              }}
+            />
+          </label>
 
-        <button type="submit">Register</button>
-      </form>
-    </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-  </>)
-}
-
-export default CreateUser
+export default CreateUser;
