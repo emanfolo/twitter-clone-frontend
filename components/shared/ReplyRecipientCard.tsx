8167bba6a76cf5c502396af7c7c9294a87d80040
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-01 17:31:27 +0000

    update to tweet reply form modal

---
+++ components/shared/ReplyRecipientCard.tsx
@@ -0,0 +1,32 @@
+import TimeAgo from "timeago-react"
+
+const ReplyRecipientCard = (props: any) => {
+
+  const {tweetInfo} = props
+
+
+  const profilePicture = tweetInfo.user.profile.image
+  const profilePictureDisplay = () => {
+  return profilePicture ? profilePicture : './DefaultImage.jpeg'
+  }
+
+
+  return <>
+
+    <div className="replyRecipientImage">
+      <img src={profilePictureDisplay()} />
+    </div>
+    <div className="replyRecipientInfo">
+      <div className="replyNameAndTime">
+        {tweetInfo.user.name} @{tweetInfo.user.username}
+        <TimeAgo datetime={props.tweetInfo.createdAt}/>  
+      </div>  
+      <div className="replyRecipientTweet" >
+        {tweetInfo.contents}
+      </div>
+    </div >
+  
+  </>
+}
+
+export default ReplyRecipientCard
--- components/shared/TweetReplyForm.tsx
+++ components/shared/TweetReplyForm.tsx
@@ -1,34 +1,33 @@
 import { TweetInfo } from "../../types/Interfaces"
 import TimeAgo from "timeago-react"
+import ReplyRecipientCard from "./ReplyRecipientCard"
+import { UserContext } from "../../pages/UserContext"
+import { useContext } from "react"
 
 const TweetReplyForm = (props: any) => {
+
+  const {user, setUser} = useContext(UserContext)
 
   const {tweetInfo} = props
 
   console.log(tweetInfo)
 
-  const profilePicture = tweetInfo.user.profile.image
+  const profilePicture = user.userDetails.profile.image
   const profilePictureDisplay = () => {
     return profilePicture ? profilePicture : './DefaultImage.jpeg'
   }
 
+
+
   return <>
-  <div className="replyRecipientCard">
-    <div className="replyRecipientImage">
-      <img src={profilePictureDisplay()} />
+  <div className="replyForm">
+    <ReplyRecipientCard tweetInfo={tweetInfo} />
+    <div className="dividerLine"> I am a divider line</div>
+    <div className="tweetReplyFormCurrentUser"> <img src={profilePictureDisplay()}/> </div>
+    <div className="replyingToInfo"> Replying to <div>@{tweetInfo.user.username}</div> </div>
+    <div className="tweetReplyForm">
+      I am the tweet reply form
     </div>
-    <div className="replyRecipientInfo">
-      <div className="replyNameAndTime">
-        <div>{tweetInfo.user.name} </div> @{tweetInfo.user.username}
-        <TimeAgo datetime={props.tweetInfo.createdAt}/>  
-      </div>  
-      <div className="replyRecipientTweet" >
-        {tweetInfo.contents}
-      </div>
-        
-      
-    </div >
-      
   </div>
   
   </>
