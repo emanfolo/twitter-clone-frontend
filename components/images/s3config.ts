d5548d511990bc43ff308380c5710ede323922e3
Emmanuel <83982264+emanfolo@users.noreply.github.com>
2022-01-04 21:56:08 +0000

    Delete s3config.ts

--- components/images/s3config.ts
+++
@@ -1,7 +0,0 @@
-export const s3Config = {
-        bucketName:  'twitter-clone-image-storage',
-        region: 'eu-west-2',
-        accessKeyId:'AKIA4MR4FJPEM6B3LTFE',
-        secretAccessKey: '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',
-        s3Url: 'https://twitter-clone-image-storage.s3.eu-west-2.amazonaws.com'     /* Optional */
-    }
