b9942f5930077482794d1c48e41bf8600d55a8d9
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-06 21:42:06 +0000

    added homepage

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,9 +2,9 @@ import { NextResponse, NextRequest } from 'next/server'
 
 
 export async function middleware(req: any) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/') {
-        return NextResponse.redirect('/explore')
-    }
-    return NextResponse.next()
+    // const { pathname } = req.nextUrl
+    // if (pathname == '/') {
+    //     return NextResponse.redirect('/explore')
+    // }
+    // return NextResponse.next()
 }
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+const Index = () => {
+  return <>
+  <h2> Welcome to Flitter!</h2>
+  </>
+}
+
+export default Index
