215753009b3a0c26de55c01356f9600cfb5a6957
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-06 21:59:10 +0000

    changed env

--- components/editprofile/s3config.ts
+++ components/editprofile/s3config.ts
@@ -1,7 +1,7 @@
 declare var process : {
   env: {
-    accessKeyId: string,
-    secretAccessKey: string
+    NEXT_PUBLIC_accessKeyId: string,
+    NEXT_PUBLIC_secretAccessKey: string
   }
 }
 
@@ -9,8 +9,8 @@ declare var process : {
 export const s3Config = {
         bucketName:  'twitter-clone-image-storage',
         region: 'eu-west-2',
-        accessKeyId: process.env.accessKeyId,
-        secretAccessKey: process.env.secretAccessKey,
+        accessKeyId: process.env.NEXT_PUBLIC_accessKeyId,
+        secretAccessKey: process.env.NEXT_PUBLIC_secretAccessKey,
         s3Url: 'https://twitter-clone-image-storage.s3.eu-west-2.amazonaws.com' 
     }
 
