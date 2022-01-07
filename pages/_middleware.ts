8dc0e481bd5844cf02282db6d26190d7350e5f32
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-07 01:47:01 +0000

    remove middleware

--- pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function middleware(req: any) {
-  // const { pathname } = req.nextUrl
-  // if (pathname == '/') {
-  //     return NextResponse.redirect('/explore')
-  // }
-  // return NextResponse.next()
-}
